f3e4a6c8443a70e71b354b87d46d2121e4a21347
Lark42 <145574331+Lark42@users.noreply.github.com>
2025-05-04 12:24:27 +0500

    3t-2.v.
    Открытие на 3000 порте

--- FrontTest/tic-tac-toe/vite.config.ts
+++ FrontTest/tic-tac-toe/vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: '0.0.0.0',
+    open: true // автоматически открывать браузер
   }
 })
